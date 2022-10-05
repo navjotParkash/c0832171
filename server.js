1e27cfa1df8e2dd99dca55845bf1916edc0ace96
Navjot Parkash <navjotparkash011997@gmail.com>
2022-10-05 17:47:00 -0400

    updating files

---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require('express')
+
+var app = express()
+
+const SERVER_PORT = 3000
+const SERVER_HOST = "localhost"
+
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded()) // for parsing application/x-www-form-urlencoded
+
+
+//http://localhost:3000/
+app.get('/', function (req, res) {
+    res.send("<h1>Home Page - Navjot Parkash C0832171</h1>")
+})
+
+
+  //http://localhost:3000/admin
+  app.get('/admin', (req, res) => {
+    res.send('Admin Homepage')
+  })
+  
+  //http://localhost:3000/user/100
+  app.get("/user/:id", (req, res)=> {
+        res.send(`User ID: ${req.params.id}`);
+      }
+  )
+
+app.listen(process.env.PORT  || SERVER_PORT, () => {
+    console.log(`Server running at http://${SERVER_HOST}:${SERVER_PORT}/`);
+})
+
